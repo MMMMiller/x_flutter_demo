d3ee1019e2f0aee8cb74b1c2e9968ba9bf3151e2
Miller <Miller@gmail.com>
2023-09-17 14:29:32 +0800

    fix： 换位置

--- flutter_module/android/app/src/main/kotlin/com/foxsofter/flutter_thrio_example/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.foxsofter.flutter_thrio_example
-
-import io.flutter.embedding.android.ThrioFlutterFragmentActivity
-
-class MainActivity: ThrioFlutterFragmentActivity() {
-    override fun shouldMoveToBack(): Boolean {
-        return true
-    }
-}
