72fcf527d8f0e417aedf34ba480cf335d56db410
Miller <Miller@gmail.com>
2023-09-17 14:27:11 +0800

    fix：iOS module移除Boost

---
+++ ios_module/FlutterBoostPro/GeneratedPluginRegistrant.m
@@ -0,0 +1,63 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_app_update/FlutterAppUpdatePlugin.h>)
+#import <flutter_app_update/FlutterAppUpdatePlugin.h>
+#else
+@import flutter_app_update;
+#endif
+
+#if __has_include(<flutter_thrio/FlutterThrioPlugin.h>)
+#import <flutter_thrio/FlutterThrioPlugin.h>
+#else
+@import flutter_thrio;
+#endif
+
+#if __has_include(<fluttertoast/FluttertoastPlugin.h>)
+#import <fluttertoast/FluttertoastPlugin.h>
+#else
+@import fluttertoast;
+#endif
+
+#if __has_include(<image_picker_ios/FLTImagePickerPlugin.h>)
+#import <image_picker_ios/FLTImagePickerPlugin.h>
+#else
+@import image_picker_ios;
+#endif
+
+#if __has_include(<path_provider_foundation/PathProviderPlugin.h>)
+#import <path_provider_foundation/PathProviderPlugin.h>
+#else
+@import path_provider_foundation;
+#endif
+
+#if __has_include(<permission_handler_apple/PermissionHandlerPlugin.h>)
+#import <permission_handler_apple/PermissionHandlerPlugin.h>
+#else
+@import permission_handler_apple;
+#endif
+
+#if __has_include(<shared_preferences_foundation/SharedPreferencesPlugin.h>)
+#import <shared_preferences_foundation/SharedPreferencesPlugin.h>
+#else
+@import shared_preferences_foundation;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterAppUpdatePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterAppUpdatePlugin"]];
+  [FlutterThrioPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterThrioPlugin"]];
+  [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [PathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"PathProviderPlugin"]];
+  [PermissionHandlerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionHandlerPlugin"]];
+  [SharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"SharedPreferencesPlugin"]];
+}
+
+@end
---
+++ ios_module/FlutterBoostPro/NativeViewController.h
@@ -0,0 +1,16 @@
+//
+//  NativeViewController.h
+//  Runner
+//
+//  Created by Miller on 2023/9/16.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NativeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
